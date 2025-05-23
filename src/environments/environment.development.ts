34ec412b9227b518eb47473f96f993e9171ffaaa
RominaMaita <u202213765@upc.edu.pe>
2025-05-23 18:20:08 -0500

    feat(main): add dependencies.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  serverBaseUrl: 'http://localhost:3000/api/v1',
+  ordersEndpointPath: '/orders',
+  dishesEndpointPath: '/dishes',
+  orderDishesEndpointPath: '/orders_dishes',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  serverBaseUrl: 'http://localhost:3000/api/v1',
+  ordersEndpointPath: '/orders',
+  dishesEndpointPath: '/dishes',
+  orderDishesEndpointPath: '/orders_dishes',
+};
