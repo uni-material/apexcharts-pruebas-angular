cb9bf2a90b7ffddeeccceb6eab19a21ff78611ea
RominaMaita <u202213765@upc.edu.pe>
2025-05-25 19:39:06 -0500

    feat(inventory): add inventory table

--- src/app/inventory/componentes/inventory-table/inventory-table.component.ts
+++ src/app/inventory/componentes/inventory-table/inventory-table.component.ts
@@ -3,13 +3,32 @@ import {Product} from '../../model/product.entity';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {ProductService} from '../../services/product.service';
 import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+  MatDatepickerModule,
+  MatDatepickerToggle
+} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-inventory-table',
   imports: [
-    FormsModule,
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerToggle,
+    MatDatepickerInput,
+    MatDatepicker
   ],
   templateUrl: './inventory-table.component.html',
   standalone: true,
